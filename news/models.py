232cb30dc6b4ae0cda09d00d3747cbae7d179b68
Quvonchbek <boboxonovmirkomil900@gmail.com>
2025-12-28 22:07:06 +0500

    YYYY

--- news/models.py
+++ news/models.py
@@ -7,4 +7,4 @@ class Post(models.Model):
     text =models.TextField()  
     author=models.ForeignKey(User, on_delete=models.CASCADE) 
     def __str__(self):
-        return self.title, 
+        return self.title
